0d93b5a179811a75248d0c72275b1ac517d2ac3c
sesebm <sesebastis@gmail.com>
2022-07-10 16:40:07 -0500

    first commit

---
+++ lista.js
@@ -0,0 +1,31 @@
+const list = [
+    {
+        id: 1
+        , tipo: 'Hoodies'
+        , titulo: 'Hoodie'
+        , descripcion: 1
+        , precio: 14.00
+        , stock: 10
+        , img: './featured1.png'
+    }
+    , {
+        id: 2
+        , tipo: 'Shirt'
+        , titulo: 'Shirt'
+        , descripcion: 2
+        , precio: 24.00
+        , stock: 15
+        , img: './featured2.png'
+    }
+    , {
+        id: 3
+        , tipo: 'Sweatshirts'
+        , titulo: 'Sweatshirt'
+        , descripcion: 3
+        , precio: 24.00
+        , stock: 20
+        , img: './featured3.png'
+    }
+    
+];
+export {list}
---
+++ main.js
@@ -0,0 +1,67 @@
+import { list } from './lista.js';
+
+function mostrarelementos() {
+    const showBox = document.getElementById("showbox");
+    let html = "";
+    for (let i = 0; i < list.length; i++) {
+        html += `<article class="productosjs">
+                 <div class="imagen_tarjeta"><img src="${list[i].img}" alt="${list[i].titulo}" class="imgtarjeta"></div>
+                 <button class="btn_agregar" data-id="${list[i].id}">+</button>
+                 <div class="info_tarjeta">
+                 
+                 <p>${list[i].titulo} </p>
+                 </div>
+                     
+        </article>`;
+    }
+    showBox.innerHTML = html;
+
+}
+/* mostrarelementos()
+let collection = [];
+let element;
+document.addEventListener("click", function (event) {
+    console.log(event.target.dataset);
+}); */
+
+/* document.querySelector('.cerrar1').addEventListener('click', function (event) {
+    document.querySelector('.checkbox').checked = false;
+});
+
+document.querySelector('.cerrar2').addEventListener('click', function (event) {
+    document.querySelector('.checkbox').checked = false;
+}); */
+
+document.querySelector('.all.barra').addEventListener('click', function (event) {
+    console.log('hola');
+    const showBox = document.getElementById("showbox");
+    let html = "";
+    for (let i = 0; i < list.length; i++) {
+        html += `<article class="productosjs">
+                     <div class="imagen_tarjeta"><img src="${list[i].img}" alt="${list[i].titulo}" class="imgtarjeta"></div>
+                     <button class="btn_agregar" data-id="${list[i].id}">+</button>
+                     <div class="info_tarjeta">
+                     
+                     <p>${list[i].titulo} </p>
+                     </div>
+                         
+            </article>`;
+    }    showBox.innerHTML = html;
+});
+
+ document.querySelector('.hoodies.barra').addEventListener('click', function (event) {
+    const showBox = document.getElementById("showbox");
+    let html = "";
+    for (let i = 0; i < list.length; i++) {
+        if(list[i].id==1){
+        html += `<article class="productosjs">
+                     <div class="imagen_tarjeta"><img src="${list[i].img}" alt="${list[i].titulo}" class="imgtarjeta"></div>
+                     <button class="btn_agregar" data-id="${list[i].id}">+</button>
+                     <div class="info_tarjeta">
+                     
+                     <p>${list[i].titulo} </p>
+                     </div>
+                         
+            </article>`;}    showBox.innerHTML = html;
+    }
+}); 
