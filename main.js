a989d46cfb7487d915ad48ebe1803322c04fdabf
sesebm <sesebastis@gmail.com>
2022-07-10 19:43:17 -0500

    .2

--- main.js
+++ main.js
@@ -17,20 +17,20 @@ function mostrarelementos() {
     showBox.innerHTML = html;
 
 }
-/* mostrarelementos()
+mostrarelementos()
 let collection = [];
 let element;
 document.addEventListener("click", function (event) {
     console.log(event.target.dataset);
-}); */
+});
 
-/* document.querySelector('.cerrar1').addEventListener('click', function (event) {
+document.querySelector('.cerrar1').addEventListener('click', function (event) {
     document.querySelector('.checkbox').checked = false;
 });
 
 document.querySelector('.cerrar2').addEventListener('click', function (event) {
     document.querySelector('.checkbox').checked = false;
-}); */
+});
 
 document.querySelector('.all.barra').addEventListener('click', function (event) {
     console.log('hola');
@@ -63,5 +63,38 @@ document.querySelector('.all.barra').addEventListener('click', function (event) 
                      </div>
                          
             </article>`;}    showBox.innerHTML = html;
+    }
+}); 
+
+document.querySelector('.shirts.barra').addEventListener('click', function (event) {
+    const showBox = document.getElementById("showbox");
+    let html = "";
+    for (let i = 0; i < list.length; i++) {
+        if(list[i].id==2){
+        html += `<article class="productosjs">
+                     <div class="imagen_tarjeta"><img src="${list[i].img}" alt="${list[i].titulo}" class="imgtarjeta"></div>
+                     <button class="btn_agregar" data-id="${list[i].id}">+</button>
+                     <div class="info_tarjeta">
+                     
+                     <p>${list[i].titulo} </p>
+                     </div>
+                         
+            </article>`;}    showBox.innerHTML = html;
+    }
+}); 
+document.querySelector('.sweatshirts.barra1').addEventListener('click', function (event) {
+    const showBox = document.getElementById("showbox");
+    let html = "";
+    for (let i = 0; i < list.length; i++) {
+        if(list[i].id==3){
+        html += `<div class="productosjs">
+                     <div class="imagen_tarjeta"><img src="${list[i].img}" alt="${list[i].titulo}" class="imgtarjeta"></div>
+                     <button class="btn_agregar" data-id="${list[i].id}"><p>+</p></button>
+                     <div class="info_tarjeta">
+                     
+                     <p>${list[i].titulo} </p>
+                     </div>
+                         
+            </div>`;}    showBox.innerHTML = html;
     }
 }); 
